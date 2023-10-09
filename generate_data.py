10bad8f9bf24dcd7610cedead375da1b13961a01
KaspervanM <kaspervanm@gmail.com>
2023-10-09 22:44:48 +0200

    generate_date is now set to always have distinguishable lines in the data.

--- generate_data.py
+++ generate_data.py
@@ -1,16 +1,15 @@
-from data_utils.data_generation import generate_data, LinearRNG, GaussianRNG, GrayscaleNumberGenerator, save_dataset
+from data_utils.data_generation import generate_data, LinearRNG, GaussianRNG, save_dataset
 import math
-import numpy as np
 
 dataset_size = 1000
 image_size = (28, 28)
 image_nr_background_colors = LinearRNG(1, 5)
-image_background_grays = GrayscaleNumberGenerator(GaussianRNG(0.5, 0.1))
+image_background_grays = GaussianRNG(0.55, 0.05, limit_left=0.45, limit_right=1)
 seed = 1
 diagonal = math.hypot(*image_size)
 line_min_length = 4 / diagonal
 line_nr_colors = LinearRNG(1, 5)
-line_grays = GrayscaleNumberGenerator(GaussianRNG(0.4, 0.05))
+line_grays = GaussianRNG(0.3, 0.05, limit_left=0, limit_right=0.44)
 line_nr_widths = LinearRNG(1, 5)
 line_widths = GaussianRNG(1 / diagonal, 0.5 / diagonal, limit_left=0, limit_right=4 / diagonal)
 prescale_image_size = (LinearRNG(image_size[0], image_size[0] * 20), LinearRNG(image_size[1], image_size[1] * 20))
@@ -20,9 +19,12 @@ dataset_list = generate_data(dataset_size, image_size, image_nr_background_color
                              prescale_image_size=prescale_image_size)
 
 save_dataset(dataset_list, f"data/dataset-{dataset_size}_size-{image_size[0]}x{image_size[1]}"
-                           f"_bg-{image_background_grays.number_generator.mean:.3f}"
-                           f"±{image_background_grays.number_generator.standard_deviation:.3f}_seed-{seed}"
-                           f"_line-{line_grays.number_generator.mean:.3f}"
-                           f"±{line_grays.number_generator.standard_deviation:.3f}"
+                           f"_bg-{image_background_grays.mean:.3f}"
+                           f"±{image_background_grays.standard_deviation:.3f}"
+                           f"({image_background_grays.limit_left:.3f}to({image_background_grays.limit_left:.3f})"
+                           f"_seed-{seed}"
+                           f"_line-{line_grays.mean:.3f}"
+                           f"±{line_grays.standard_deviation:.3f}"
+                           f"({line_grays.limit_left:.3f}to({line_grays.limit_left:.3f})"
                            f"_width-{line_widths.mean:.3f}"
                            f"±{line_widths.standard_deviation:.3f}")
